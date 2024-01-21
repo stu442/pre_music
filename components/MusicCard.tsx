c4d06064b4a357b8ae579e4f8e9a5db728f7587c
stu442 <stu442@naver.com>
2024-01-21 17:37:32 +0900

    feat : Image(MusicCard 컴포넌트) 스켈레톤 UI 추가

--- components/MusicCard.tsx
+++ components/MusicCard.tsx
@@ -15,12 +15,22 @@ export default function MusicCard({title, artist, img} : MusicCardProps) {
     return (
         <Dialog>
             <DialogTrigger className='flex flex-col items-center cursor-pointer'>
-                <Image width={320} height={320} src={img} alt='music_img' />
+                <Image 
+                width={320} height={320} 
+                src={img} alt='music_img' 
+                placeholder='blur' 
+                blurDataURL='data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkkAQAAB8AG7jymN8AAAAASUVORK5CYII='
+                />
                 <h3 className='mt-2'>{title}</h3>
                 <p className='text-sm text-black/70'>{artist}</p>
             </DialogTrigger>
             <DialogContent>
-                <Image width={640} height={640} src={img} alt='music_img' />
+                <Image 
+                width={640} height={640} 
+                src={img} alt='music_img' 
+                placeholder='blur' 
+                blurDataURL='data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkkAQAAB8AG7jymN8AAAAASUVORK5CYII='
+                />
             </DialogContent>
         </Dialog>
     )
