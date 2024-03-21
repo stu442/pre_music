4df3da1b5cfba0ead7d5487eb39375a036ec5692
GJG <stu442@naver.com>
2024-03-21 18:59:02 +0900

    refactor : action 리팩터링, Base64 Fetching 로직 Custom Hooks 으로 분리

--- app/action.tsx
+++ app/action.tsx
@@ -1,26 +1,25 @@
 'use server'
 
+import { errorToast } from "@/components/ui/use-toast";
 import { getPlaiceholder } from "plaiceholder";
-
-export async function imgTobase64(src:string) {
-    try {
-      const buffer = await fetch(src).then(async (res) =>
-        Buffer.from(await res.arrayBuffer())
-      );
-      const { base64 } = await getPlaiceholder(buffer);
-      return base64
-    } catch (err) {
-      throw new Error("이미지를 불러오는 중 에러가 발생했습니다.")
-    }
-  }
 
 export async function imgTobase64s(musicList : SpotifyTrack[]) {
   try {
-    const imgUrls = musicList.map(music => music.album.images[0].url);
-    const promises = imgUrls.map(imgUrl => imgTobase64(imgUrl));
-    const base64s = await Promise.all(promises);
+    if (!Array.isArray(musicList)) {
+      throw new Error("유효하지 않은 musicList입니다.");
+    }
+
+    const base64s = await Promise.all(
+      musicList.map(async (music) => {
+        const src = music.album.images[0].url;
+        const response = await fetch(src);
+        const buffer = Buffer.from(await response.arrayBuffer());
+        const { base64 } = await getPlaiceholder(buffer);
+        return base64;
+      })
+    );
     return base64s;
   } catch (err) {
-    throw new Error("이미지를 불러오는 중 에러가 발생했습니다.")
+    throw errorToast(err);
   }
 }
--- components/HomeMusicList.tsx
+++ components/HomeMusicList.tsx
@@ -7,7 +7,7 @@ import MusicCardOnClient from "./MusicCardOnClient"
 import { fetchTrackData } from "@/api/fetchTrackData"
 import { useEffect, useState } from "react"
 import { errorToast } from "./ui/use-toast"
-import { imgTobase64s } from "@/app/action"
+import { useFetchBase64s } from "@/hooks/useFetchBase64"
 
 interface HomeMusicListProps {
     title: string
@@ -17,7 +17,7 @@ interface HomeMusicListProps {
 export default function HomeMuisicList({title, contents_id} : HomeMusicListProps) {
 
     const [trackData, setTrackData] = useState<SpotifyTracks>()
-    const [base64, setBase64] = useState<(string | undefined)[]>([])
+    const base64 = useFetchBase64s(trackData)
 
     useEffect(() => {
       if (contents_id && contents_id.length > 0) {
@@ -29,20 +29,8 @@ export default function HomeMuisicList({title, contents_id} : HomeMusicListProps
             errorToast(error)
           }
         };
-
-        const fetchBase64s = async () => {
-          try {
-            if(trackData) {
-              const base64s = await imgTobase64s(trackData.tracks);
-              setBase64(base64s);
-            }
-          } catch(error) {
-            errorToast(error)
-          }
-        };
         
         fetchData();
-        fetchBase64s();
       }
     }, [contents_id]);
     
