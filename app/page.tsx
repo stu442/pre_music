0c7defe2e37addd294a7affb4d69f345781cd81b
GJG <stu442@naver.com>
2024-03-21 18:15:48 +0900

    refactor : page에서 Fetch로직 Custom hooks로 분리

--- app/page.tsx
+++ app/page.tsx
@@ -1,110 +1,14 @@
 'use client'
 
-import { AccessTokenProps, getAccessToken } from "@/api/getAceessToken";
 import HomeMuisicList from "@/components/HomeMusicList";
 import Spinner from "@/components/Spinner";
-import { errorToast } from "@/components/ui/use-toast";
-import { supabase } from "@/lib/utils";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { useFetchTrackData } from "@/hooks/useFetchTrackData";
 
-interface ContentsItem {
-  contents_id: string
-}
-
-interface GroupedContents {
-  [key: string]: number
-}
 
 export default function Home() {
-
-  const [mostLikedMusic, setMostLikedMusic] = useState<string[] | null>([])
-  const [recentMusic, setRecentMusic] = useState<string[] | null>([])
-  const [adminMusic, setAdminMusic] = useState<string[] | null>([])
-
-  const FETCHNUM = 10
-
-  useEffect(() => {
-    function sortLikedMusic(data : ContentsItem[] | null) {
-      if(data === null) {
-        setMostLikedMusic(null)
-        return
-      } 
-      const groupedContents:GroupedContents = {};
-        data.forEach((item) => {
-          if (!groupedContents[item.contents_id]) {
-            groupedContents[item.contents_id] = 0;
-          }
-          groupedContents[item.contents_id]++;
-        });
-        const sortedContents = (Object.entries(groupedContents))
-        .sort((a, b) => b[1] - a[1])
-        .slice(0, FETCHNUM)
-        .map(([contentsId]) => contentsId);
-        setMostLikedMusic(sortedContents);
-    }
-    async function fetchMusicFromTable(tableName: string, setMusicCallback: Dispatch<SetStateAction<string[] | null>>) {
-      try {
-        const { data, error } = await supabase
-          .from(tableName)
-          .select('contents_id')
-          .order('liked_at', { ascending: false })
-          .limit(FETCHNUM);
-        if (error) {
-          throw new Error('음악을 불러오는데 실패했습니다.');
-        }
-        setMusicCallback(extractedIds(data));
-      } catch (error) {
-        errorToast(error);
-      }
-    }
-    async function fetchSortedMostLikedMusic() {
-      try {
-        const { data, error } = await supabase
-          .from('LIKES')
-          .select('contents_id')
-          .order('liked_at', { ascending: false })
-          .limit(FETCHNUM);
-    
-        if (error) {
-          throw new Error('음악을 불러오는데 실패했습니다.');
-        }
-        sortLikedMusic(data);
-      } catch (error) {
-        errorToast(error);
-      }
-    }
-    
-    async function fetchRecentMusic() {
-      await fetchMusicFromTable('LIKES', setRecentMusic);
-    }
-    
-    async function fetchAdminMusic() {
-      await fetchMusicFromTable('ADMIN_LIKES', setAdminMusic);
-    }
-    
-    (async () => {
-        await initAccessToken()
-        await fetchSortedMostLikedMusic()
-        await fetchRecentMusic()
-        await fetchAdminMusic()
-    })()
-  }, [])
-
-  async function initAccessToken() {
-    let accessToken: AccessTokenProps | null = JSON.parse(localStorage.getItem('spotifyAccessToken') || 'null');
-    if(accessToken === null) {
-      accessToken = await getAccessToken();
-      localStorage.setItem('spotifyAccessToken', JSON.stringify(accessToken));
-    }
-  }
-
-  function extractedIds(data : ContentsItem[] | null) {
-    if(data === null) {
-      return null
-    }
-    const extractedData = data.map(item => item.contents_id);
-    return extractedData
-  }
+  const { track: adminMusic } = useFetchTrackData("ADMIN");
+  const { track: mostLikedMusic } = useFetchTrackData("MOSTLIKED");
+  const { track: recentMusic } = useFetchTrackData("RECENT");
 
   return (
     <>
---
+++ hooks/useFetchTrackData.ts
@@ -0,0 +1,63 @@
+import { useState, useEffect } from 'react';
+import { supabase } from '@/lib/utils';
+import { errorToast } from '@/components/ui/use-toast';
+
+interface ContentsItem {
+    contents_id: string
+}
+
+type MusicType = 'ADMIN' | 'RECENT' | 'MOSTLIKED'
+  
+export const useFetchTrackData = (type : MusicType) => {
+  const FETCHNUM = 10
+  const [track, setTrack] = useState<string[]>([])
+
+  function extractedIds(data : ContentsItem[]) {
+    if(data === null) {
+      return []
+    }
+    const extractedData = data.map(item => item.contents_id);
+    return extractedData
+  }
+
+  useEffect(() => {
+    const fetchMusic = async () => {
+      try {
+        let data, error;
+        switch (type) {
+          case 'ADMIN':
+            ({ data, error } = await supabase
+              .from('ADMIN_LIKES')
+              .select('contents_id')
+              .order('liked_at', { ascending: false })
+              .limit(FETCHNUM));
+            break;
+          case 'RECENT':
+            ({ data, error } = await supabase
+              .from('LIKES')
+              .select('contents_id')
+              .order('liked_at', { ascending: false })
+              .limit(FETCHNUM));
+            break;
+          case 'MOSTLIKED':
+            ({ data, error } = await supabase
+              .rpc('get_most_liked_contents')
+              .limit(FETCHNUM));
+            break;
+          default:
+            throw new Error('Invalid type');
+        }
+        if (error) {
+          throw new Error('음악을 불러오는데 실패했습니다.');
+        }
+        setTrack(extractedIds(data));
+      } catch (error) {
+        errorToast(error)
+      }
+    };
+
+    fetchMusic();
+  }, [type]);
+
+  return { track };
+};
