82e0f9ba7262e607a71ce624ad5bc04b558bb72f
GJG <stu442@naver.com>
2024-03-27 16:13:58 +0900

    refactor : AudioPlayer mouse 이벤트, Touch 이벤트 합치기

--- components/AudioPlayer.tsx
+++ components/AudioPlayer.tsx
@@ -10,7 +10,10 @@ interface AudioPlayerProps {
 }
 
 export default function AudioPlayer({ musicUrl, testId } : AudioPlayerProps) {
-    const [volume, setVolume] = useState(0.4);
+    const DEFAULT_VOLUME = 0.4;
+    const SPEAKER_DURATION = 1000;
+
+    const [volume, setVolume] = useState(DEFAULT_VOLUME);
     const [isVisible, setIsVisible] = useState(false);
     const audioRef = useRef<HTMLAudioElement>(null);
     const timerId = useRef<NodeJS.Timeout | null>(null);
@@ -35,38 +38,25 @@ export default function AudioPlayer({ musicUrl, testId } : AudioPlayerProps) {
 
     }, [volume, handleMusicUrlError])
 
-    const playPauseToggleMouse = () => {
-
+    const playPauseToggle = (e: React.MouseEvent<HTMLButtonElement> | React.TouchEvent<HTMLButtonElement>) => {
+      if ('touches' in e) {
+        e.preventDefault();
+      }
       handleMusicUrlError();
-      if(timerId.current) {
+      if (timerId.current) {
         clearTimeout(timerId.current);
       }
       setIsVisible(true);
       timerId.current = setTimeout(() => {
         setIsVisible(false);
-      }, 1000);
-
-      setVolume((prevVolume) => (prevVolume === 0 ? 0.4 : 0));
+      }, SPEAKER_DURATION);
+      setVolume((prevVolume) => (prevVolume === 0 ? DEFAULT_VOLUME : 0));
     };
     
-    const playPauseToggleTouch = (e:React.TouchEvent<HTMLButtonElement>) => {
-
-      e.preventDefault()
-      handleMusicUrlError();
-      if(timerId.current) {
-        clearTimeout(timerId.current);
-      }
-      setIsVisible(true);
-      timerId.current = setTimeout(() => {
-        setIsVisible(false);
-      }, 1000);
-
-      setVolume((prevVolume) => (prevVolume === 0 ? 0.4 : 0));
-    };
     
     return (
         <>
-              <button className='flex justify-center items-center w-full h-full fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]' onClick={playPauseToggleMouse} onTouchEnd={e => playPauseToggleTouch(e)} data-cy={`audio-btn-${testId}`}>
+              <button className='flex justify-center items-center w-full h-full fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]' onClick={playPauseToggle} onTouchEnd={e => playPauseToggle(e)} data-cy={`audio-btn-${testId}`}>
               </button>
                 {volume === 0 ? (
                   isVisible && <Image style={{zIndex:1000, pointerEvents:'none'}} className='bg-white/10 p-2 rounded-full transition duration-500 ease-linear fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]' src="/icons/speaker_off.svg" alt="음소거" width={120} height={120} data-cy={`audio-off-${testId}`} />
