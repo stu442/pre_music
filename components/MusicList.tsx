8f702656d60bedb194585fbb1470987346026e76
GJG <stu442@naver.com>
2024-03-27 16:15:47 +0900

    Refactor : MusicList Fetch 로직 Custom hooks로 합침

--- components/MusicList.tsx
+++ components/MusicList.tsx
@@ -1,34 +1,16 @@
 'use client'
 
-import { fetchSearchData } from '@/api/fetchSearchData';
-import { useEffect } from 'react'
 import MusicCardOnClient from './MusicCardOnClient';
 import IntersectionObserverComponent from './IntersectionObserverComponents';
-import { useRecoilState } from 'recoil';
 import Spinner from './Spinner';
-import { imgTobase64s } from '@/app/action';
-import { base64State, loadingState, musicListState, nextLinkState } from '@/atom/musicListAtom';
+import { useFetchMusicList } from '@/hooks/useFetchMusicList';
 
 interface MusicListProps {
     keyword: string
 }
 
 export default function MusicList({keyword} : MusicListProps) {
-    const [musicList, setMusicList] = useRecoilState<SpotifyTrack[]>(musicListState);
-    const [nextLink, setNextLink] = useRecoilState<string | null>(nextLinkState);
-    const [isLoading, setIsLoading] = useRecoilState<boolean>(loadingState);
-    const [base64, setBase64] = useRecoilState<(string | undefined)[]>(base64State);
-    
-    useEffect(() => {
-        (async () => {
-            const { tracks } = await fetchSearchData(keyword, 'track');
-            setMusicList(tracks.items);
-            setNextLink(tracks.next);
-            setIsLoading(false);
-            const base64s = await imgTobase64s(tracks.items);
-            setBase64(base64s)
-        })();
-    }, [keyword, setMusicList, setNextLink, setIsLoading, setBase64])
+    const { musicList, isLoading, base64 } = useFetchMusicList(keyword)
 
     return (
         <>
@@ -38,7 +20,7 @@ export default function MusicList({keyword} : MusicListProps) {
             <>
                 {musicList.map((item: SpotifyTrack, id: number) => (
                 <MusicCardOnClient
-                    key={`music_card_${id}`}
+                    key={`music_card_${item.id}`}
                     title={item.name}
                     artist={item.album.artists[0].name}
                     imgUrl={item.album.images[0].url}
