92b9095e858081a9f1e800d4da012e4d79c62c22
GJG <stu442@naver.com>
2024-03-27 16:07:40 +0900

    refactor : TODO 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import HomeMuisicList from "@/components/HomeMusicList";
-import Spinner from "@/components/Spinner";
 import { useFetchContentsId } from "@/hooks/useFetchContentsId";
-
+import Spinner from "@/components/Spinner";
 
 export default function Home() {
   const { contentsId: adminMusic } = useFetchContentsId("ADMIN");
@@ -12,12 +11,14 @@ export default function Home() {
 
   return (
     <>
-      {!adminMusic?.length && !mostLikedMusic?.length && !recentMusic?.length && (
-        <Spinner />
-      )}
-      {<HomeMuisicList title="주인장 추천 음악" contentsId={adminMusic} />}
-      {<HomeMuisicList title="회원님들의 픽" contentsId={mostLikedMusic} />}
-      {<HomeMuisicList title="최근 좋아요 받은 음악" contentsId={recentMusic} />}
+    {/* TODO : Loading 제대로 다시 구현해보기 */}
+    {/* HomeMusicList에서 로딩 중에도 로딩이 나오도록... */}
+    {/* 아니면 스켈레톤 UI를 추가하는 방식으로 가도 너무 좋을 것 같다. */}
+    {/* Loading State를 추가하고, */}
+      {!(adminMusic?.length || mostLikedMusic?.length || recentMusic?.length) && <Spinner />}
+        <HomeMuisicList title="주인장 추천 음악" contentsId={adminMusic} />
+        <HomeMuisicList title="회원님들의 픽" contentsId={mostLikedMusic} />
+        <HomeMuisicList title="최근 좋아요 받은 음악" contentsId={recentMusic} />
     </>
   )
 }
