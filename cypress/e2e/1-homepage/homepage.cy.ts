2ba87fa8be24f8977b04259f30a22be7ec9ff8fc
GJG <stu442@naver.com>
2024-03-27 16:06:02 +0900

    test : Test 수정사항 수정

--- cypress/e2e/1-homepage/homepage.cy.ts
+++ cypress/e2e/1-homepage/homepage.cy.ts
@@ -7,11 +7,13 @@ describe("홈페이지 요소 확인하기", () => {
             "token_type": "Bearer",
             "expires_in": 3600
         })
+        cy.intercept({method:"POST", url: "https://yzxeirrpyitzfbkwddhe.supabase.co/rest/v1/rpc/get_most_liked_contents?limit=10"},{fixture:"homepage/supabase_getMostLiked.json"})
         cy.intercept({method:"GET", url: "https://yzxeirrpyitzfbkwddhe.supabase.co/rest/v1/ADMIN_LIKES?select=contents_id&order=liked_at.desc&limit=10"},{fixture:"homepage/supabase_admin_response.json"})
         cy.intercept({method:"GET", url: "https://yzxeirrpyitzfbkwddhe.supabase.co/rest/v1/LIKES?select=contents_id&order=liked_at.desc&limit=10"},{fixture:"homepage/supabase_response.json"})
         cy.intercept({method:"GET", url: "https://yzxeirrpyitzfbkwddhe.supabase.co/rest/v1/LIKES?select=contents_id&order=liked_at.desc&limit=10"},{fixture:"homepage/supabase_response.json"})
         cy.intercept({method:"GET", url : "https://api.spotify.com/v1/tracks/?ids=6luBKkFUt5wTwz7hpLhp12,4AFsRbaLKRWo3dDtjDFA2V,3P8LJzc8QBtHqmLQCyoCQe,3iVcZ5G6tvkXZkZKlMpIUs,0POWf265mo16skF8KwzIMn,3xKsf9qdS1CyvXSMEid6g8,7DfFc7a6Rwfi3YQMRbDMau,5GKwq4sO5ZHKuWaDmdwMQc,1SmwJJ8YVgiXtyE6elhUx1,5HshJOG3lOUEZinx0I3vXf"}, {fixture : "homepage/onlyTrackData.json"})
         cy.intercept({method:"GET", url : "https://api.spotify.com/v1/tracks/?ids=01MoBCPgZHsOPYwMJxc9Qo,1G8ZiodFtDX5tNHwvxcllW,3ZMPl8BrHqX0erYjqWiZ0c,7tr2za8SQg2CI8EDgrdtNl,74g2fPpMSMMH3DinOoTWUB,3UZSpAlc0WZjxkfbUBdUgT,4vLYewWIvqHfKtJDk8c8tq,2oCJgHbwGnYAKXenorXMpW,5DG1ux5rYimpUxMPh2HQcz,6f5c2TrHJxscSjX1CVFlfR"}, {fixture : "homepage/trackData.json"})
+        cy.intercept({method:"GET", url : "https://api.spotify.com/v1/tracks/?ids=01MoBCPgZHsOPYwMJxc9Qo,3YDuuc8zxwDHVNCeLHRKms,0JJeoiCAa1hwcBsPxBN2w4,6xGruZOHLs39ZbVccQTuPZ,1G8ZiodFtDX5tNHwvxcllW,6rY5FAWxCdAGllYEOZMbjW,6SB7by6S98iScUgybNS3PI,4Dwsvu6JgyAH78GfdlUxBh,5DG1ux5rYimpUxMPh2HQcz,4X7R14O8tuvGlb0ieHyDYA        "}, {fixture : "homepage/trackData.json"})
     })
     it("홈페이지 음악 카드 클릭과 오디오 버튼 동작 확인", () => {
         // when - 특정 음악 카드를 클릭했을 때
